2c3ea07bcbcad6f5f588eabb949d4d57352a6fc0
mirdayo <mir6066@gmail.com>
2023-08-04 11:52:07 +0900

    230804 Spring

--- 02CONTROLLER_VALIDATION/src/main/java/com/test/app/controller/MemoController.java
+++ 02CONTROLLER_VALIDATION/src/main/java/com/test/app/controller/MemoController.java
@@ -3,7 +3,9 @@ package com.test.app.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,14 +21,22 @@ public class MemoController {
 	
 	@GetMapping("/form")
 	public void get() {
-		log.info("GET /memo/get...");
+		log.info("GET /memo/form...");
 	}
 	@PostMapping("/post")
-	public void post(@Valid MemoDto dto, BindingResult bindingResult) {
-		//유효성 체크
-		log.info("POST /memo/post..." + dto);
-		log.info("POST /memo/post...isError" + bindingResult.getFieldError("id"));
-		if(bindingResult.hasFieldErrors())
-			log.info("POST /memo/post...isError" + bindingResult.getFieldError("id").getDefaultMessage());
+	public String post(@Valid MemoDto dto,BindingResult bindingResult,Model model) {
+		log.info("POST /memo/post... "+dto);
+		log.info("POST /memo/post... isError "+bindingResult.getFieldError("id"));
+		if(bindingResult.hasFieldErrors()) {
+//			log.info("POST /memo/post... isError "+bindingResult.getFieldError("id").getDefaultMessage());
+			for( FieldError error  : bindingResult.getFieldErrors()) {
+				log.info(error.getField()+ " : " + error.getDefaultMessage());
+				model.addAttribute(error.getField(), error.getDefaultMessage());		
+			}
+			return "memo/form";
+		}
+			
+		return "";
+		
 	}
 }
---
+++ 03CONTROLLER_EXCEPTION/src/test/java/com/test/app/DiTests2.java
@@ -0,0 +1,23 @@
+package com.test.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.test.app.controller.MemoController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class DiTests2 {
+	
+	@Autowired
+	MemoController controller;
+
+	@Test
+	public void test() {
+		System.out.println("SimpleController : " + controller);
+	}
+
+}
